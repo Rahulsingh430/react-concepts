f599af71deda1ed181b21dd5208ab41edc2e2284
Rahulsingh430 <er.rahulchonker@gmail.com>
2022-11-22 23:31:41 +0530

    added api 1by1

--- appps/src/app1.jsx
+++ appps/src/app1.jsx
@@ -6,23 +6,29 @@ let LOGINUSERKEY = "LOGINUSERKEY";
 function App1() {
   const [count, setCount] = useState(0);
   const [user, setUser] = useState([]);
-  useEffect(() => {
-    const key = JSON.parse(localStorage.getItem("LOGINUSERKEY"));
-    console.log(key);
-  });
-
+  const [num, setNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   const key = JSON.parse(localStorage.getItem("LOGINUSERKEY"));
+  //   console.log(key);
+  // });
+  const handleget = () => {
+    setNum(num + 1);
+  };
   useEffect(() => {
     setTimeout(() => {
       setCount((count) => count + 1);
     }, 1000);
   }, []);
   const get = async () => {
+    setLoading(true);
     const { data } = await Axios.get(
-      "https://jsonplaceholder.typicode.com/todos/1"
+      `https://jsonplaceholder.typicode.com/photos/${num}`
     );
     setUser(data);
+    setLoading(false);
     localStorage.setItem("LOGINUSERKEY", JSON.stringify(data));
-    // console.log(data);
+    console.log(data);
   };
   useEffect(() => {
     // Axios.get("https://jsonplaceholder.typicode.com/todos/1").then(
@@ -32,15 +38,22 @@ function App1() {
     //   }
     // );
     get();
-  }, []);
+  }, [num]);
+  if (loading) {
+    return <div>...Loading</div>;
+  }
 
   return (
     <>
-      <h1>I've rendered {count} times!</h1>;
+      <h1>I've rendered {count} times!</h1>
+      <button onClick={handleget}>Get image</button>
+      {/* {user.map((x) => ( */}
       <div>
-        <p>{user.userId}</p>
+        <h1>{user.userId}</h1>
         <h1>{user.title}</h1>
+        <img src={user.url} />
       </div>
+      {/* ))} */}
     </>
   );
 }
